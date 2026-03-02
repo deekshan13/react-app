4b0def7e3814ce9003f1ac8489b3c570c6ebe34a
Deeksha <deeksha.n@setty.com>
2026-03-02 12:50:38 +0530

    updated the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from "react";
-import TodoItem from "./components/TodoItem";
-import { dummyData } from "./data/todos";
 import AddTodoForm from "./components/AddTodoForm";
 import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import useTodos from "./hooks/useTodos";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
-
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo)),
-    );
-  }
-  function addTodo(title: string) {
-    setTodos((prevTodos) => [
-      {
-        id: prevTodos.length + 1,
-        title,
-        completed: false,
-      },
-      ...prevTodos,
-    ]);
-  }
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteAllCompletedTodos,
+    deleteTodo,
+  } = useTodos();
   return (
-    <main className="py-8 h-screen space-y-5">
+    <main className="py-8 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center ">Your ToDos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoForm onSubmit={addTodo} />
-        <TodoList todos={todos} onCompletedChange={setTodoCompleted} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+        />
       </div>
+      <TodoSummary todos={todos} deleteAllCompleted={deleteAllCompletedTodos} />
     </main>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,14 +1,19 @@
 import type { Todo } from "../types/todos";
-
+import { Trash2 } from "lucide-react";
 interface TodoItemProps {
   todo: Todo;
   onCompletedChange: (id: number, completed: boolean) => void;
+  onDelete: (id: number) => void;
 }
 
-export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
+export default function TodoItem({
+  todo,
+  onCompletedChange,
+  onDelete,
+}: TodoItemProps) {
   return (
-    <div>
-      <label className="flex items-center gap-2 rounded-md p-2 border-gray-400 bg-white  hover:bg-slate-50 ">
+    <div className="flex items-center gap-1">
+      <label className="flex items-center gap-2 rounded-md p-2 border-gray-400 bg-white  hover:bg-slate-50 grow ">
         <input
           type="checkbox"
           checked={todo.completed}
@@ -19,6 +24,9 @@ export default function TodoItem({ todo, onCompletedChange }: TodoItemProps) {
           {todo.title}
         </span>
       </label>
+      <button onClick={() => onDelete(todo.id)} className="p-2">
+        <Trash2 size={20} className="text-gray-500" />
+      </button>
     </div>
   );
 }
